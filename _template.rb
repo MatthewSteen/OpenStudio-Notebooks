81ada5286a305ffb04f076fe1791ca09f73f95c4
MatthewSteen <matt@ambient-e.com>
2015-12-29 13:53:44 -0700

    initial commit

---
+++ _template.rb
@@ -0,0 +1,13 @@
+require 'openstudio'
+
+file_name = "OsmName.osm"
+file_name_new = "OsmNameNew.osm"
+
+model_in_path = "#{File.dirname(__FILE__)}/#{file_name}"
+model = OpenStudio::Model::Model.load(model_in_path).get
+model_out_path = "#{File.dirname(__FILE__)}/#{file_name_new}"
+
+# do stuff
+
+
+model.save(model_out_path,true)
